994752e37af64b33159646733db85db4613025b0
Amrit <amritpal6239@outlook.com>
2025-05-10 11:39:17 +0530

    improved formatting

--- includes/helper.h
+++ includes/helper.h
@@ -2,16 +2,18 @@
 #define HELPER_H
 int helper_num_places(int n);
 #include "log.h"
-#define NOTIMPLEMENTED(message)                                                                 \
-    do                                                                                \
-    {                                                                                 \
-        fprintf(stderr, BOLDRED"NOT IMPLEMENTD"RESET":"message" at %s:%d in %s\n", __FILE__, __LINE__, __func__); \
-        exit(1);                                                                      \
+#define NOTIMPLEMENTED(message)                                                         \
+    do                                                                                  \
+    {                                                                                   \
+        fprintf(stderr, BOLDRED "NOT IMPLEMENTD" RESET ":" message " at %s:%d in %s\n", \
+                __FILE__, __LINE__, __func__);                                          \
+        exit(1);                                                                        \
     } while (0)
-#define NOTREACHABLE                                                                  \
-    do                                                                               \
-    {                                                                                \
-        fprintf(stderr, BOLDRED"NOT REACHABLE"RESET": %s:%d in %s\n", __FILE__, __LINE__, __func__); \
-        exit(1);                                                                     \
+#define NOTREACHABLE                                                     \
+    do                                                                   \
+    {                                                                    \
+        fprintf(stderr, BOLDRED "NOT REACHABLE" RESET ": %s:%d in %s\n", \
+                __FILE__, __LINE__, __func__);                           \
+        exit(1);                                                         \
     } while (0)
 #endif
